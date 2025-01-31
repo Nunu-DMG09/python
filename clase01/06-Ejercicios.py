779a006633be618055b757050d8711894c20339e
Nunu-DMG09 <davidmesta09@gmail.com>
2025-01-31 17:03:30 -0500

    ejercicios

---
+++ clase01/06-Ejercicios.py
@@ -0,0 +1,35 @@
+# EJERCICIOS 
+
+# EJERCICIO 1:
+# ESCRIBE UN PROGRAMA QUE IMPRIMA TU NOMBRE Y TU CIUDAD EN LINEA SEPARADAS
+
+# CODIGO AQUI
+
+
+# EJERCICIO 2:
+# Usa el comando 'type()' para determinar el tipo de dato de cada variable
+
+# CODIGO AQUI
+
+
+# EJERCICIO 3:
+# Convierte la cadena "12345" a un entero y luego a un float.
+# Convierte el float 3.99 a un entero. ¿Que ocurre?
+
+# CODIGO AQUI
+
+
+# EJERCICIO 4:
+# Crea variables para tu nombre, edad y altura
+# Usa f-strings para imprimir  una presentacion
+
+# CODIGO AQUI
+
+
+# EJERCICIO 5:
+# Crea variables con el número PI (sin asignar una variable)
+# Luego redondea el numero con round()
+# Haz la division entera entre el numero que te salio y el numero 2 
+# El resultado deberia ser 1 
+
+# CODIGO AQUI
